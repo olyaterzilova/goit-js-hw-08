0ce23f954636c1020b60a75c21b7526fbf132452
olyaterzilova <Terzilova31@gmail.com>
2023-08-17 18:35:25 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,11 @@
 import Player from '@vimeo/player';
-const throttle = require('lodash.throttle');
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
 // Використовуємо lodash.throttle для оновлення часу не частіше 1 разу на секунду
-const updateLocalStorage = throttle(function(event) {
+const updateLocalStorage = throttle(function (event) {
   localStorage.setItem('videoplayer-current-time', event.seconds);
 }, 1000);
 
@@ -15,4 +15,4 @@ player.on('timeupdate', updateLocalStorage);
 let resLocalStorage = localStorage.getItem('videoplayer-current-time');
 
 // Встановлюємо значення з localStorage
-player.setCurrentTime( (resLocalStorage === null) ? 0 : resLocalStorage );
+player.setCurrentTime(resLocalStorage || 0);
