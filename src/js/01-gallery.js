61fcf74a2cc4c951b9c3fee18bb8205178d54655
olyaterzilova <Terzilova31@gmail.com>
2023-08-23 13:29:28 +0300

    111

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,12 +1,10 @@
 // Описаний в документації
-import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from 'simplelightbox';
 // Додатковий імпорт стилів
-import "simplelightbox/dist/simple-lightbox.min.css";
-
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 
 // Функція для створення розмітки елемента галереї
 function createGalleryItem(item) {
@@ -24,33 +22,13 @@ function renderGallery() {
   const galleryList = document.querySelector('.gallery');
 
   const galleryMarkup = galleryItems.map(createGalleryItem).join('');
-    galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
-    
-    // Використовуэмо бібліотеку галереї
-    new SimpleLightbox('.gallery a');
+  galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
 }
 
 // Рендеримо галерею при завантаженні сторінки
-window.addEventListener('DOMContentLoaded', renderGallery);
+renderGallery();
 
-
-
-// // Додаємо обробник події на ul.gallery для делегування кліків на елементи галереї
-// document.querySelector('.gallery').addEventListener('click', onGalleryItemClick);
-
-// // Функція для обробки кліку на елемент галереї
-// function onGalleryItemClick(event) {
-//   event.preventDefault();
-
-//   // Перевіряємо, чи був клік на зображенні
-//   if (event.target.nodeName !== 'IMG') {
-//     return;
-//   }
-
-//   // Отримуємо посилання на велике зображення
-//   const largeImageURL = event.target.parentNode.getAttribute('href');
-
-//   // Відкриваємо модальне вікно з великим зображенням
-//   const instance = basicLightbox.create(`<img src="${largeImageURL}" width="800" height="600">`);
-//   instance.show();
-// }
+// Використовуэмо бібліотеку галереї
+new SimpleLightbox('.gallery a', {
+  sourceAttr: 'href',
+});
